ad5644363dc6db0f26176993b9f7c0b3359f84d0
Lamburger <rival2h@gmail.com>
2017-04-12 02:01:08 -0700

    break

---
+++ app.js
@@ -0,0 +1,61 @@
+var quiz = [
+            {
+                "question"		: 	"Q1: What is the relationship between Ana and Phara?",
+                "choices"		: 	[
+                    "They're Sisters",
+                    "They are Cousins",
+                    "They're not related!",
+                    "Mother and daughter"
+                ],
+                "correct"		: 	"Mother and daughter",
+                "explanation"	: 	"Ana is Pharah's mother.",
+
+            },
+            {
+                "question"		: 	"Q2: When do heroes die?",
+                "choices"		: 	[
+                    "High noon",
+                    "Never",
+                    "Right now",
+                    "Tomorrow"
+                ],
+                "correct"		: 	"Never",
+                "explanation"	: 	"This is Mercy's line",
+            },
+            {
+                "question"		: 	"Q3: Where does justice rain from?",
+                "choices"		: 	[
+                    "Pharah",
+                    "the Sky",
+                    "Above",
+                    "The Clouds"
+                ],
+                "correct"		: 	"Above",
+                "explanation"	: 	"Pharah yells this during her ult.",
+            },
+ {
+                "question"		: 	"Q4: This character is British Nationality",
+                "choices"		: 	[
+                    "Pharah",
+                    "Widowmaker",
+                    "Tracer",
+                    "D.Va"
+                ],
+                "correct"		: 	"Tracer",
+                "explanation"	: 	"he was once a test pilot for an experimental time-traveling aircraft, but was lost in time due to the effects of the experiment going poorly.",
+            },
+             {
+                "question"		: 	"Q5: Which character is a giant German who wields a rocket-powered hammer and giant shield?",
+                "choices"		: 	[
+                    "Ana",
+                    "Reaper",
+                    "Reinhardt",
+                    "Lucio"
+                ],
+                "correct"		: 	"Reinhardt",
+                "explanation"	: 	"He was once a member of the German Crusaders during the Omnic Crisis.",
+            },
+
+
+
+        ];
