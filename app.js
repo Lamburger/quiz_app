162ee1808931a10f700b8a19d6398a5e8660d0ad
Lamburger <rival2h@gmail.com>
2017-04-12 02:30:09 -0700

    JS question changes

--- app.js
+++ app.js
@@ -1,61 +1,22 @@
-var quiz = [
-            {
-                "question"		: 	"Q1: What is the relationship between Ana and Phara?",
-                "choices"		: 	[
-                    "They're Sisters",
-                    "They are Cousins",
-                    "They're not related!",
-                    "Mother and daughter"
-                ],
-                "correct"		: 	"Mother and daughter",
-                "explanation"	: 	"Ana is Pharah's mother.",
-
-            },
-            {
-                "question"		: 	"Q2: When do heroes die?",
-                "choices"		: 	[
-                    "High noon",
-                    "Never",
-                    "Right now",
-                    "Tomorrow"
-                ],
-                "correct"		: 	"Never",
-                "explanation"	: 	"This is Mercy's line",
-            },
-            {
-                "question"		: 	"Q3: Where does justice rain from?",
-                "choices"		: 	[
-                    "Pharah",
-                    "the Sky",
-                    "Above",
-                    "The Clouds"
-                ],
-                "correct"		: 	"Above",
-                "explanation"	: 	"Pharah yells this during her ult.",
-            },
- {
-                "question"		: 	"Q4: This character is British Nationality",
-                "choices"		: 	[
-                    "Pharah",
-                    "Widowmaker",
-                    "Tracer",
-                    "D.Va"
-                ],
-                "correct"		: 	"Tracer",
-                "explanation"	: 	"he was once a test pilot for an experimental time-traveling aircraft, but was lost in time due to the effects of the experiment going poorly.",
-            },
-             {
-                "question"		: 	"Q5: Which character is a giant German who wields a rocket-powered hammer and giant shield?",
-                "choices"		: 	[
-                    "Ana",
-                    "Reaper",
-                    "Reinhardt",
-                    "Lucio"
-                ],
-                "correct"		: 	"Reinhardt",
-                "explanation"	: 	"He was once a member of the German Crusaders during the Omnic Crisis.",
-            },
-
-
-
-        ];
+(function() {
+  var questions = [{
+    question: "What is the relationship between Ana and Phara?",
+    choices: ["They're Sisters", "They are Cousins", "They're not related!", "Mother and daughter",],
+    correctAnswer: "Mother and daughter"
+  }, {
+    question: "When do heroes die?",
+    choices: ["High noon", "Never", "Right now", "Tomorrow",],
+    correctAnswer: "Never"
+  }, {
+    question: "Where does justice rain from?",
+    choices: ["Pharah", "the Sky", "Above", "The Clouds",],
+    correctAnswer: "Above"
+  }, {
+    question: "This character is British Nationality",
+    choices: ["Pharah", "Widowmaker", "Tracer", "D.Va",],
+    correctAnswer: "Tracer"
+  }, {
+    question: "Which character is a giant German who wields a rocket-powered hammer and giant shield?",
+    choices: ["Ana", "Reaper", "Reinhardt", "Lucio",],
+    correctAnswer: "Reinhardt"
+  }];
